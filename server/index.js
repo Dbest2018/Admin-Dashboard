c979a8466dc820e955d86835ea50f0df64add1de
Dam <shit2wice@gmail.com>
2023-01-24 21:31:36 +0100

    fix: Insert large data into database

--- server/index.js
+++ server/index.js
@@ -12,7 +12,19 @@ import salesRoutes from "./routes/sales.js";
 
 // data imports
 import User from "./models/User.js";
-import { dataUser } from "./data/index.js";
+import Product from "./models/Product.js";
+import ProductStat from "./models/ProductStat.js";
+import Transaction from "./models/Transaction.js";
+import OverallStat from "./models/OverallStat.js";
+import AffiliateStat from "./models/AffiliateStat.js";
+import {
+  dataUser,
+  dataProduct,
+  dataProductStat,
+  dataTransaction,
+  dataOverallStat,
+  dataAffiliateStat,
+} from "./data/index.js";
 
 /* CONFIGURATION */
 dotenv.config();
